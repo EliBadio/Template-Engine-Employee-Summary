dadd123f9726e95c0fab493570c9871dc971c3a4
Elisha Badio <elisha.badio@uconn.edu>
2020-10-12 12:50:21 -0400

    update apps file and the class file input

---
+++ Develop/app.js
@@ -0,0 +1,176 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+const teamList = [];
+
+const createManager = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter manager's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter manager's id?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter manager's email?"
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Enter manager's office number?"
+        }
+    ]).then(answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+
+        //console.log(manager)
+
+        // Add manager to team list array
+        teamList.push(manager)
+
+        addTeamMemeber();
+    })
+}
+
+const addTeamMemeber = () => {
+    // Ask what who to add
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'type',
+            message: 'Which type of team member would you like to add?',
+            choices: [
+                'Engineer',
+                'Intern',
+                "I don't want to add any more member"
+            ]
+        }
+    ]).then(answer => {
+        if(answer.type === 'Engineer') {
+            createEngineer()
+        } else if(answer.type === 'Intern'){
+            createIntern()
+        } else {
+            console.log(teamList)
+            generateHtmlFile()
+        }
+    })
+}
+
+const createEngineer = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your engineer's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is your engineer's id?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your engineer's email?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is your engineer's github?"
+        },
+    ]).then(answers => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+
+        teamList.push(engineer);
+
+        addTeamMemeber();
+    })
+}
+
+const createIntern = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your intern's name?" 
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is your intern's id?" 
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your intern's email?" 
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is your intern's school?" 
+        },
+    ]).then(answers => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+
+        teamList.push(intern);
+
+        addTeamMemeber();
+    })
+}
+
+const generateHtmlFile = () => {
+    // call render function to return html block 
+    const HTML = render(teamList);
+
+    // create output folder if it doesn't already exist
+    if(!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+
+    // create html file using html returned from the render function
+    fs.writeFileSync(outputPath, HTML, err => {
+        if(err) {
+            console.log(err)
+        }
+    })
+}
+
+createManager()
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
