404bec35ee12464497be40b381f65fb5f136aeca
Elisha Badio <elisha.badio@uconn.edu>
2020-10-14 17:40:48 -0400

    commmit style.css

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,5 +1,6 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
+
 class Manager extends Employee {
     constructor( name, email, id, officeNumber) {
        super(name, id, email);
